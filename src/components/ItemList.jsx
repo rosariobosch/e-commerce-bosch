5ac1758a0e76dbecb83c21470354f0a1661a5d24
Rosario Bosch <rosariobosch93@gmail.com>
2021-08-12 19:46:44 -0300

    ItemList & ItemListContainer

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,35 +1,9 @@
 import Item from './Item';
-import Products from "./prodctos.json"
-import { useState } from 'react';
 
-const ItemList = () => {
+const ItemList = ({items}) => {
 
-    const productArr = Products
-
-    const [products, setProducts] = useState([]);
-
-
-    const promise = new Promise ((resolve, reject) => {
-
-        setTimeout(() => {
-                resolve(productArr)
-                reject('No products')
-        }, 2000);
-    })
-
-    
-    promise.then(() => {
-        if (products.length === 0) setProducts(products.concat(productArr))   
-            
-
-    }).catch((err) => {
-        console.log(err + ' were found')
-    })
-
-    return <div className='item-list'>{products.map(product => <Item key={product.id} name={product.name} img={product.img} price={product.price}/>)}</div>
+    return <div className='item-list'>{items.map(product => <Item key={product.id} name={product.name} img={product.img} price={product.price}/>)}</div>
 
 }
-
-
 
 export default ItemList;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,33 @@
 
 import ItemList from "./ItemList";
+import Products from "./prodctos.json"
+import { useState } from 'react';
 
 const ItemListContainer = () => {
 
-    return <ItemList />
+    const productArr = Products
+
+    const [products, setProducts] = useState([]);
+
+
+    const promise = new Promise ((resolve, reject) => {
+
+        setTimeout(() => {
+                resolve(productArr)
+                reject('No products')
+        }, 2000);
+    })
+
+    
+    promise.then(() => {
+        if (products.length === 0) setProducts(productArr)   
+            
+
+    }).catch((err) => {
+        console.log(err + ' were found')
+    })
+
+    return <ItemList items = {products} />
 }
 
 export default ItemListContainer;

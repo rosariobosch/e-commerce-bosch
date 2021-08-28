a1bdc4bb31ae5feaef95ced446eab2412903f82d
Rosario Bosch <rosariobosch93@gmail.com>
2021-08-28 19:44:29 -0300

    Firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDD03FC--K40QR-Y6_szPp5iHp-NY5wylA",
+  authDomain: "e-commerce-bosch.firebaseapp.com",
+  projectId: "e-commerce-bosch",
+  storageBucket: "e-commerce-bosch.appspot.com",
+  messagingSenderId: "606528617927",
+  appId: "1:606528617927:web:2530c86b0a3c9231deade2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
